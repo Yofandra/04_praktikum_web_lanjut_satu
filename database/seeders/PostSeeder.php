6c8a6500cf90f10a30ce39bb4ea092e18407d892
Yofandra <drayofan@gmail.com>
2023-03-13 11:14:04 +0700

    update1

---
+++ database/seeders/PostSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('posts')->insert([
+            'nama'=> 'Yofandra Arta Priyoga',
+            'pendidikan'=> 'Mahasiswa Polinema',
+            'asal'=> 'AG people',
+            'funfact'=> 'Gak suka pedes'         
+        ]);
+        
+    }
+}
